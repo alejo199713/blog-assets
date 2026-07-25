9741f8694bd69571c8f10da29332261149ae0426
alejo199713 <alejo1997131@gmail.com>
2026-07-25 12:51:04 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -85,9 +85,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         heroCard.classList.add("expanded");
 
-        heroCard.style.background =
-        'url("https://alejo199713.github.io/blog-assets/assets/ui/Hero_Expanded.svg") center/100% 100% no-repeat';
-
         projectsCard.classList.add("hidden");
 
     });
